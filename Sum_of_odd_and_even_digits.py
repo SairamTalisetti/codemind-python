bff04e58ad8f907d966e29be6be4ad827a0abb81
SairamTalisetti <106660045+SairamTalisetti@users.noreply.github.com>
2022-08-13 22:05:32 +0530

    x=int(input()) l=list(map(int,input().split())) e=0 o=0 for i in range(x):     if i%2==0:         e+=l[i]     else:         o+=l[i] if e-o==0:     print("YES") else:     print("NO")

---
+++ Sum_of_odd_and_even_digits.py
@@ -0,0 +1,13 @@
+x=int(input())
+l=list(map(int,input().split()))
+e=0
+o=0
+for i in range(x):
+    if i%2==0:
+        e+=l[i]
+    else:
+        o+=l[i]
+if e-o==0:
+    print("YES")
+else:
+    print("NO")
